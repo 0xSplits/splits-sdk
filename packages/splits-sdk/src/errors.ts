f880a31ef0eaf946a604b7c6b23f63e77eae72dd
Mike H <mikeh09@gmail.com>
2022-08-16 08:27:04 -0700

    add prototype calls to error classes

--- packages/splits-sdk/src/errors.ts
+++ packages/splits-sdk/src/errors.ts
@@ -3,6 +3,9 @@ import {
   SPLITS_SUPPORTED_CHAIN_IDS,
 } from './constants'
 
+// Manually setting the prototype in the constructor with setPrototypeOf fixes a typescript issue so that the
+// unit tests can detect the error class
+
 export class UnsupportedChainIdError extends Error {
   name = 'UnsupportedChainIdError'
 
@@ -10,6 +13,7 @@ export class UnsupportedChainIdError extends Error {
     super(
       `Unsupported chain: ${invalidChainId}. Supported chains are: ${SPLITS_SUPPORTED_CHAIN_IDS}`,
     )
+    Object.setPrototypeOf(this, UnsupportedChainIdError.prototype)
   }
 }
 
@@ -20,37 +24,78 @@ export class UnsupportedSubgraphChainIdError extends Error {
     super(
       `Unsupported subgraph chain. Supported subgraph chains are: ${SPLITS_SUBGRAPH_CHAIN_IDS}`,
     )
+    Object.setPrototypeOf(this, UnsupportedSubgraphChainIdError.prototype)
   }
 }
 
 export class InvalidRecipientsError extends Error {
   name = 'InvalidRecipientsError'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, InvalidRecipientsError.prototype)
+  }
 }
 
 export class InvalidDistributorFeePercentError extends Error {
   name = 'InvalidDistributorFeePercent'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, InvalidDistributorFeePercentError.prototype)
+  }
 }
 
 export class InvalidArgumentError extends Error {
   name = 'InvalidArgumentError'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, InvalidArgumentError.prototype)
+  }
 }
 
 export class InvalidAuthError extends Error {
   name = 'InvalidAuthError'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, InvalidAuthError.prototype)
+  }
 }
 
 export class TransactionFailedError extends Error {
   name = 'TransactionFailedError'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, TransactionFailedError.prototype)
+  }
 }
 
 export class MissingProviderError extends Error {
   name = 'MissingProviderError'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, MissingProviderError.prototype)
+  }
 }
 
 export class MissingSignerError extends Error {
   name = 'MissingSignerError'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, MissingSignerError.prototype)
+  }
 }
 
 export class InvalidConfigError extends Error {
   name = 'InvalidConfigError'
+
+  constructor(m?: string) {
+    super(m)
+    Object.setPrototypeOf(this, InvalidConfigError.prototype)
+  }
 }
