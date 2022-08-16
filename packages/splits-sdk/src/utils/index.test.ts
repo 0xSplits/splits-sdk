c12c4a451d38b1da4bdcf16a96ecdbf073fdb767
Mike H <mikeh09@gmail.com>
2022-08-16 08:27:23 -0700

    validation and helper unit tests

---
+++ packages/splits-sdk/src/utils/index.test.ts
@@ -0,0 +1,68 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import { getRecipientSortedAddressesAndAllocations } from './index'
+
+describe('Test sorting recipients', () => {
+  test('Recipients come back sorted by address', () => {
+    const recipients = [
+      {
+        address: '456',
+        percentAllocation: 456,
+      },
+      {
+        address: '789',
+        percentAllocation: 789,
+      },
+      {
+        address: '123',
+        percentAllocation: 123,
+      },
+    ]
+
+    const [sortedAddresses, sortedPercentAllocations] =
+      getRecipientSortedAddressesAndAllocations(recipients)
+
+    const expectedAddresses = ['123', '456', '789']
+    const expectedPercentAllocations = [
+      BigNumber.from(123 * 1e4),
+      BigNumber.from(456 * 1e4),
+      BigNumber.from(789 * 1e4),
+    ]
+
+    expect(sortedAddresses).toEqual(expectedAddresses)
+    expect(sortedPercentAllocations).toEqual(expectedPercentAllocations)
+  })
+  test('Sorting by lowercase values', () => {
+    const recipients = [
+      {
+        address: 'ad',
+        percentAllocation: 3,
+      },
+      {
+        address: 'Ae',
+        percentAllocation: 4,
+      },
+      {
+        address: 'ab',
+        percentAllocation: 1,
+      },
+      {
+        address: 'Ac',
+        percentAllocation: 2,
+      },
+    ]
+
+    const [sortedAddresses, sortedPercentAllocations] =
+      getRecipientSortedAddressesAndAllocations(recipients)
+
+    const expectedAddresses = ['ab', 'Ac', 'ad', 'Ae']
+    const expectedPercentAllocations = [
+      BigNumber.from(1 * 1e4),
+      BigNumber.from(2 * 1e4),
+      BigNumber.from(3 * 1e4),
+      BigNumber.from(4 * 1e4),
+    ]
+
+    expect(sortedAddresses).toEqual(expectedAddresses)
+    expect(sortedPercentAllocations).toEqual(expectedPercentAllocations)
+  })
+})
---
+++ packages/splits-sdk/src/utils/validation.test.ts
@@ -0,0 +1,90 @@
+import {
+  InvalidRecipientsError,
+  InvalidDistributorFeePercentError,
+  InvalidArgumentError,
+} from '../errors'
+import { SplitRecipient } from '../types'
+import {
+  validateRecipients,
+  validateDistributorFeePercent,
+  validateAddress,
+} from './validation'
+
+describe('Address validation', () => {
+  test('Invalid address fails', () => {
+    const address = '12345'
+    expect(() => validateAddress(address)).toThrow(InvalidArgumentError)
+  })
+  test('Valid address passes', () => {
+    const address = '0xF8843981e7846945960f53243cA2Fd42a579f719'
+    expect(() => validateAddress(address)).not.toThrow()
+  })
+})
+
+describe('Distributor fee validation', () => {
+  test('Outside valid range fails', () => {
+    expect(() => validateDistributorFeePercent(-0.1)).toThrow(
+      InvalidDistributorFeePercentError,
+    )
+    expect(() => validateDistributorFeePercent(10.01)).toThrow(
+      InvalidDistributorFeePercentError,
+    )
+  })
+  test('Too many decimals fails', () => {
+    expect(() => validateDistributorFeePercent(0.12345)).toThrow(
+      InvalidDistributorFeePercentError,
+    )
+  })
+  test('Valid distributor fees pass', () => {
+    expect(() => validateDistributorFeePercent(0)).not.toThrow()
+    expect(() => validateDistributorFeePercent(10)).not.toThrow()
+    expect(() => validateDistributorFeePercent(4.1234)).not.toThrow()
+  })
+})
+
+describe('Recipients validation', () => {
+  let recipients: SplitRecipient[]
+  beforeEach(() => {
+    recipients = [
+      {
+        address: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
+        percentAllocation: 50,
+      },
+      {
+        address: '0xec8bfc8637247cee680444ba1e25fa5e151ba342',
+        percentAllocation: 50,
+      },
+    ]
+  })
+
+  test('Percent allocation doesnt sum to 100 fails', () => {
+    recipients[0].percentAllocation = 51
+    expect(() => validateRecipients(recipients)).toThrow(InvalidRecipientsError)
+  })
+  test('Percent allocationt too many decimals fails', () => {
+    recipients[0].percentAllocation = 49.99999
+    recipients[1].percentAllocation = 50.00001
+    expect(() => validateRecipients(recipients)).toThrow(InvalidRecipientsError)
+  })
+  test('Percent allocation outside valid range fails', () => {
+    recipients[0].percentAllocation = 0
+    recipients[1].percentAllocation = 100
+    expect(() => validateRecipients(recipients)).toThrow(InvalidRecipientsError)
+  })
+  test('Repeat address fails', () => {
+    recipients[1].address = recipients[0].address
+    expect(() => validateRecipients(recipients)).toThrow(InvalidRecipientsError)
+  })
+  test('Less than two recipients fails', () => {
+    expect(() => validateRecipients(recipients.slice(0, 1))).toThrow(
+      InvalidRecipientsError,
+    )
+  })
+  test('Invalid address fails', () => {
+    recipients[0].address = '12345'
+    expect(() => validateRecipients(recipients)).toThrow(InvalidRecipientsError)
+  })
+  test('Valid recipients pass', () => {
+    expect(() => validateRecipients(recipients)).not.toThrow()
+  })
+})
