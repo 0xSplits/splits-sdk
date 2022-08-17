49cb99ee6376c5127b945ba2d916b22e4adf6a23
Mike H <mikeh09@gmail.com>
2022-08-17 11:52:25 -0700

    initial set of client tests

---
+++ packages/splits-sdk/src/client.test.ts
@@ -0,0 +1,152 @@
+import { Provider } from '@ethersproject/abstract-provider'
+import { Signer } from '@ethersproject/abstract-signer'
+import { Contract } from '@ethersproject/contracts'
+import type { Event } from '@ethersproject/contracts'
+
+import { SplitsClient } from './client'
+import {
+  InvalidConfigError,
+  MissingProviderError,
+  MissingSignerError,
+  UnsupportedChainIdError,
+} from './errors'
+import * as utils from './utils'
+import {
+  validateRecipients,
+  validateDistributorFeePercent,
+  validateAddress,
+} from './utils/validation'
+
+jest.mock('@ethersproject/contracts', () => {
+  return {
+    Contract: () => {
+      return {
+        provider: 1,
+
+        interface: {
+          getEvent: () => {
+            return {
+              format: () => {
+                return 'format'
+              },
+            }
+          },
+        },
+
+        connect: () => {
+          return {
+            createSplit: () => 'create_split_tx',
+            updateSplit: () => 'update_split_tx',
+          }
+        },
+
+        getController: () => {
+          return 'controllerAddress'
+        },
+      }
+    },
+  }
+})
+jest.mock('./utils/validation')
+
+const getTransactionEventSpy = jest
+  .spyOn(utils, 'getTransactionEvent')
+  .mockImplementation(async () => {
+    const event = {
+      blockNumber: 12345,
+      args: {
+        split: '0xsplit',
+      },
+    } as unknown as Event
+    return event
+  })
+
+const mockProvider = jest.fn<Provider, void[]>()
+const mockSigner = jest.fn<Signer, void[]>(() => {
+  return {
+    getAddress: () => {
+      return 'controllerAddress'
+    },
+  } as unknown as Signer
+})
+
+describe('Client config validation', () => {
+  test('Including ens names with no provider fails', () => {
+    expect(
+      () => new SplitsClient({ chainId: 1, includeEnsNames: true }),
+    ).toThrow(InvalidConfigError)
+  })
+
+  test('Invalid chain id fails', () => {
+    expect(() => new SplitsClient({ chainId: 51 })).toThrow(
+      UnsupportedChainIdError,
+    )
+  })
+
+  test('Ethereum chain ids pass', () => {
+    expect(() => new SplitsClient({ chainId: 1 })).not.toThrow()
+    expect(() => new SplitsClient({ chainId: 3 })).not.toThrow()
+    expect(() => new SplitsClient({ chainId: 4 })).not.toThrow()
+    expect(() => new SplitsClient({ chainId: 5 })).not.toThrow()
+    expect(() => new SplitsClient({ chainId: 42 })).not.toThrow()
+  })
+
+  test('Polygon chain ids pass', () => {
+    expect(() => new SplitsClient({ chainId: 137 })).not.toThrow()
+    expect(() => new SplitsClient({ chainId: 80001 })).not.toThrow()
+  })
+})
+
+describe('SplitMain writes', () => {
+  const provider = new mockProvider()
+  const signer = new mockSigner()
+  const splitsClient = new SplitsClient({
+    chainId: 1,
+    provider,
+    signer,
+  })
+
+  beforeEach(() => {
+    ;(validateRecipients as jest.Mock).mockClear()
+    ;(validateDistributorFeePercent as jest.Mock).mockClear()
+    ;(validateAddress as jest.Mock).mockClear()
+    getTransactionEventSpy.mockClear()
+
+    expect(validateRecipients).not.toBeCalled()
+    expect(validateDistributorFeePercent).not.toBeCalled()
+    expect(validateAddress).not.toBeCalled()
+    expect(getTransactionEventSpy).not.toBeCalled()
+  })
+
+  test('Create split passes', async () => {
+    const recipients = [{ address: '0xuser', percentAllocation: 45 }]
+    const distributorFeePercent = 7.35
+    const { event, splitId } = await splitsClient.createSplit({
+      recipients,
+      distributorFeePercent,
+    })
+
+    expect(event.blockNumber).toEqual(12345)
+    expect(splitId).toEqual('0xsplit')
+    expect(validateRecipients).toBeCalledWith(recipients)
+    expect(validateDistributorFeePercent).toBeCalledWith(distributorFeePercent)
+    expect(getTransactionEventSpy).toBeCalledWith('create_split_tx', 'format')
+  })
+
+  test('Update split passes', async () => {
+    const splitId = '0xabc'
+    const recipients = [{ address: '0xhey', percentAllocation: 12 }]
+    const distributorFeePercent = 9
+    const { event } = await splitsClient.updateSplit({
+      splitId,
+      recipients,
+      distributorFeePercent,
+    })
+
+    expect(event.blockNumber).toEqual(12345)
+    expect(validateAddress).toBeCalledWith(splitId)
+    expect(validateRecipients).toBeCalledWith(recipients)
+    expect(validateDistributorFeePercent).toBeCalledWith(distributorFeePercent)
+    expect(getTransactionEventSpy).toBeCalledWith('update_split_tx', 'format')
+  })
+})
