286c3e6bef07c8d695697aa76f463994a293fb94
Mike H <mikeh09@gmail.com>
2022-09-01 20:32:07 -0700

    add back common types

--- packages/splits-sdk/src/typechain/SplitMain/polygon/SplitMain.d.ts
+++ packages/splits-sdk/src/typechain/SplitMain/polygon/SplitMain.d.ts
@@ -17,7 +17,7 @@ import {
 import { BytesLike } from "@ethersproject/bytes";
 import { Listener, Provider } from "@ethersproject/providers";
 import { FunctionFragment, EventFragment, Result } from "@ethersproject/abi";
-import { TypedEventFilter, TypedEvent, TypedListener } from "./commons";
+import { TypedEventFilter, TypedEvent, TypedListener } from "../../commons";
 
 interface SplitMainInterface extends ethers.utils.Interface {
   functions: {
