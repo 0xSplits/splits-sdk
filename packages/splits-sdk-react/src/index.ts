075b24e3e853ecdd0966641bd80362c0d3a2b46f
Mike H <mikeh09@gmail.com>
2022-10-28 15:35:12 -0700

    export types from splits core sdk

--- packages/splits-sdk-react/src/index.ts
+++ packages/splits-sdk-react/src/index.ts
@@ -1,3 +1,10 @@
+export type {
+  SplitsClientConfig,
+  SplitsClient,
+  Split,
+  WaterfallModule,
+  LiquidSplit,
+} from '@0xsplits/splits-sdk'
 export { SplitsProvider } from './context'
 export {
   useSplitsClient,
