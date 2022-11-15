e525ac4a74f84b94531dde812dd83cbbae4b2d77
Mike H <mikeh09@gmail.com>
2022-11-15 12:07:33 -0800

    add waterfall and liquid split hooks

--- packages/splits-sdk-react/src/hooks/liquid.ts
+++ packages/splits-sdk-react/src/hooks/liquid.ts
@@ -1,7 +1,171 @@
-import { useContext, useEffect, useState } from 'react'
-import { LiquidSplit } from '@0xsplits/splits-sdk'
+import { useCallback, useContext, useEffect, useState } from 'react'
+import type { Event } from '@ethersproject/contracts'
+import {
+  getTransactionEvents,
+  CreateLiquidSplitConfig,
+  LiquidSplit,
+  DistributeLiquidSplitTokenConfig,
+  TransferLiquidSplitOwnershipConfig,
+} from '@0xsplits/splits-sdk'
 
 import { SplitsContext } from '../context'
+import { ContractExecutionStatus, RequestError } from '../types'
+
+export const useCreateLiquidSplit = (): {
+  createLiquidSplit: (
+    arg0: CreateLiquidSplitConfig,
+  ) => Promise<Event[] | undefined>
+  status?: ContractExecutionStatus
+  txHash?: string
+  error?: RequestError
+} => {
+  const context = useContext(SplitsContext)
+  if (context === undefined) {
+    throw new Error('Make sure to include <SplitsProvider>')
+  }
+
+  const [status, setStatus] = useState<ContractExecutionStatus>()
+  const [txHash, setTxHash] = useState<string>()
+  const [error, setError] = useState<RequestError>()
+
+  const createLiquidSplit = useCallback(
+    async (argsDict: CreateLiquidSplitConfig) => {
+      if (!context.splitsClient.liquidSplits)
+        throw new Error('Invalid chain id for liquid splits')
+
+      try {
+        setStatus('pendingApproval')
+
+        const { tx } =
+          await context.splitsClient.liquidSplits.submitCreateLiquidSplitTransaction(
+            argsDict,
+          )
+
+        setStatus('txInProgress')
+        setTxHash(tx.hash)
+
+        const events = await getTransactionEvents(
+          tx,
+          context.splitsClient.liquidSplits.eventTopics.createLiquidSplit,
+        )
+
+        setStatus('complete')
+
+        return events
+      } catch (e) {
+        setStatus('error')
+        setError(e)
+      }
+    },
+    [context.splitsClient],
+  )
+
+  return { createLiquidSplit, status, txHash, error }
+}
+
+export const useDistributeLiquidSplitToken = (): {
+  distributeToken: (
+    arg0: DistributeLiquidSplitTokenConfig,
+  ) => Promise<Event[] | undefined>
+  status?: ContractExecutionStatus
+  txHash?: string
+  error?: RequestError
+} => {
+  const context = useContext(SplitsContext)
+  if (context === undefined) {
+    throw new Error('Make sure to include <SplitsProvider>')
+  }
+
+  const [status, setStatus] = useState<ContractExecutionStatus>()
+  const [txHash, setTxHash] = useState<string>()
+  const [error, setError] = useState<RequestError>()
+
+  const distributeToken = useCallback(
+    async (argsDict: DistributeLiquidSplitTokenConfig) => {
+      if (!context.splitsClient.liquidSplits)
+        throw new Error('Invalid chain id for liquid splits')
+
+      try {
+        setStatus('pendingApproval')
+
+        const { tx } =
+          await context.splitsClient.liquidSplits.submitDistributeTokenTransaction(
+            argsDict,
+          )
+
+        setStatus('txInProgress')
+        setTxHash(tx.hash)
+
+        const events = await getTransactionEvents(
+          tx,
+          context.splitsClient.liquidSplits.eventTopics.distributeToken,
+        )
+
+        setStatus('complete')
+
+        return events
+      } catch (e) {
+        setStatus('error')
+        setError(e)
+      }
+    },
+    [context.splitsClient],
+  )
+
+  return { distributeToken, status, txHash, error }
+}
+
+export const useTransferLiquidSplitOwnership = (): {
+  transferOwnership: (
+    arg0: TransferLiquidSplitOwnershipConfig,
+  ) => Promise<Event[] | undefined>
+  status?: ContractExecutionStatus
+  txHash?: string
+  error?: RequestError
+} => {
+  const context = useContext(SplitsContext)
+  if (context === undefined) {
+    throw new Error('Make sure to include <SplitsProvider>')
+  }
+
+  const [status, setStatus] = useState<ContractExecutionStatus>()
+  const [txHash, setTxHash] = useState<string>()
+  const [error, setError] = useState<RequestError>()
+
+  const transferOwnership = useCallback(
+    async (argsDict: TransferLiquidSplitOwnershipConfig) => {
+      if (!context.splitsClient.liquidSplits)
+        throw new Error('Invalid chain id for liquid splits')
+
+      try {
+        setStatus('pendingApproval')
+
+        const { tx } =
+          await context.splitsClient.liquidSplits.submitTransferOwnershipTransaction(
+            argsDict,
+          )
+
+        setStatus('txInProgress')
+        setTxHash(tx.hash)
+
+        const events = await getTransactionEvents(
+          tx,
+          context.splitsClient.liquidSplits.eventTopics.transferOwnership,
+        )
+
+        setStatus('complete')
+
+        return events
+      } catch (e) {
+        setStatus('error')
+        setError(e)
+      }
+    },
+    [context.splitsClient],
+  )
+
+  return { transferOwnership, status, txHash, error }
+}
 
 export const useLiquidSplitMetadata = (
   liquidSplitId: string,
--- packages/splits-sdk-react/src/hooks/waterfall.ts
+++ packages/splits-sdk-react/src/hooks/waterfall.ts
@@ -1,7 +1,222 @@
-import { useContext, useEffect, useState } from 'react'
-import { WaterfallModule } from '@0xsplits/splits-sdk'
+import { useCallback, useContext, useEffect, useState } from 'react'
+import type { Event } from '@ethersproject/contracts'
+import {
+  CreateWaterfallConfig,
+  getTransactionEvents,
+  RecoverNonWaterfallFundsConfig,
+  WaterfallFundsConfig,
+  WaterfallModule,
+  WithdrawWaterfallPullFundsConfig,
+} from '@0xsplits/splits-sdk'
 
 import { SplitsContext } from '../context'
+import { ContractExecutionStatus, RequestError } from '../types'
+
+export const useCreateWaterfallModule = (): {
+  createWaterfallModule: (
+    arg0: CreateWaterfallConfig,
+  ) => Promise<Event[] | undefined>
+  status?: ContractExecutionStatus
+  txHash?: string
+  error?: RequestError
+} => {
+  const context = useContext(SplitsContext)
+  if (context === undefined) {
+    throw new Error('Make sure to include <SplitsProvider>')
+  }
+
+  const [status, setStatus] = useState<ContractExecutionStatus>()
+  const [txHash, setTxHash] = useState<string>()
+  const [error, setError] = useState<RequestError>()
+
+  const createWaterfallModule = useCallback(
+    async (argsDict: CreateWaterfallConfig) => {
+      if (!context.splitsClient.waterfall)
+        throw new Error('Invalid chain id for waterfall')
+
+      try {
+        setStatus('pendingApproval')
+
+        const { tx } =
+          await context.splitsClient.waterfall.submitCreateWaterfallModuleTransaction(
+            argsDict,
+          )
+
+        setStatus('txInProgress')
+        setTxHash(tx.hash)
+
+        const events = await getTransactionEvents(
+          tx,
+          context.splitsClient.waterfall.eventTopics.createWaterfallModule,
+        )
+
+        setStatus('complete')
+
+        return events
+      } catch (e) {
+        setStatus('error')
+        setError(e)
+      }
+    },
+    [context.splitsClient],
+  )
+
+  return { createWaterfallModule, status, txHash, error }
+}
+
+export const useWaterfallFunds = (): {
+  waterfallFunds: (arg0: WaterfallFundsConfig) => Promise<Event[] | undefined>
+  status?: ContractExecutionStatus
+  txHash?: string
+  error?: RequestError
+} => {
+  const context = useContext(SplitsContext)
+  if (context === undefined) {
+    throw new Error('Make sure to include <SplitsProvider>')
+  }
+
+  const [status, setStatus] = useState<ContractExecutionStatus>()
+  const [txHash, setTxHash] = useState<string>()
+  const [error, setError] = useState<RequestError>()
+
+  const waterfallFunds = useCallback(
+    async (argsDict: WaterfallFundsConfig) => {
+      if (!context.splitsClient.waterfall)
+        throw new Error('Invalid chain id for waterfall')
+
+      try {
+        setStatus('pendingApproval')
+
+        const { tx } =
+          await context.splitsClient.waterfall.submitWaterfallFundsTransaction(
+            argsDict,
+          )
+
+        setStatus('txInProgress')
+        setTxHash(tx.hash)
+
+        const events = await getTransactionEvents(
+          tx,
+          context.splitsClient.waterfall.eventTopics.waterfallFunds,
+        )
+
+        setStatus('complete')
+
+        return events
+      } catch (e) {
+        setStatus('error')
+        setError(e)
+      }
+    },
+    [context.splitsClient],
+  )
+
+  return { waterfallFunds, status, txHash, error }
+}
+
+export const useRecoverNonWaterfallFunds = (): {
+  recoverNonWaterfallFunds: (
+    arg0: RecoverNonWaterfallFundsConfig,
+  ) => Promise<Event[] | undefined>
+  status?: ContractExecutionStatus
+  txHash?: string
+  error?: RequestError
+} => {
+  const context = useContext(SplitsContext)
+  if (context === undefined) {
+    throw new Error('Make sure to include <SplitsProvider>')
+  }
+
+  const [status, setStatus] = useState<ContractExecutionStatus>()
+  const [txHash, setTxHash] = useState<string>()
+  const [error, setError] = useState<RequestError>()
+
+  const recoverNonWaterfallFunds = useCallback(
+    async (argsDict: RecoverNonWaterfallFundsConfig) => {
+      if (!context.splitsClient.waterfall)
+        throw new Error('Invalid chain id for waterfall')
+
+      try {
+        setStatus('pendingApproval')
+
+        const { tx } =
+          await context.splitsClient.waterfall.submitRecoverNonWaterfallFundsTransaction(
+            argsDict,
+          )
+
+        setStatus('txInProgress')
+        setTxHash(tx.hash)
+
+        const events = await getTransactionEvents(
+          tx,
+          context.splitsClient.waterfall.eventTopics.recoverNonWaterfallFunds,
+        )
+
+        setStatus('complete')
+
+        return events
+      } catch (e) {
+        setStatus('error')
+        setError(e)
+      }
+    },
+    [context.splitsClient],
+  )
+
+  return { recoverNonWaterfallFunds, status, txHash, error }
+}
+
+export const useWithdrawWaterfallPullFunds = (): {
+  withdrawPullFunds: (
+    arg0: WithdrawWaterfallPullFundsConfig,
+  ) => Promise<Event[] | undefined>
+  status?: ContractExecutionStatus
+  txHash?: string
+  error?: RequestError
+} => {
+  const context = useContext(SplitsContext)
+  if (context === undefined) {
+    throw new Error('Make sure to include <SplitsProvider>')
+  }
+
+  const [status, setStatus] = useState<ContractExecutionStatus>()
+  const [txHash, setTxHash] = useState<string>()
+  const [error, setError] = useState<RequestError>()
+
+  const withdrawPullFunds = useCallback(
+    async (argsDict: WithdrawWaterfallPullFundsConfig) => {
+      if (!context.splitsClient.waterfall)
+        throw new Error('Invalid chain id for waterfall')
+
+      try {
+        setStatus('pendingApproval')
+
+        const { tx } =
+          await context.splitsClient.waterfall.submitWithdrawPullFundsTransaction(
+            argsDict,
+          )
+
+        setStatus('txInProgress')
+        setTxHash(tx.hash)
+
+        const events = await getTransactionEvents(
+          tx,
+          context.splitsClient.waterfall.eventTopics.withdrawPullFunds,
+        )
+
+        setStatus('complete')
+
+        return events
+      } catch (e) {
+        setStatus('error')
+        setError(e)
+      }
+    },
+    [context.splitsClient],
+  )
+
+  return { withdrawPullFunds, status, txHash, error }
+}
 
 export const useWaterfallMetadata = (
   waterfallModuleId: string,
