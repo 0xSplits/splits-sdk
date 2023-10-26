c9799687b3f0a8eac860f3e3b69bf11ae80b5f13
Jonathan Cai <caimjonathan@gmail.com>
2023-10-26 13:49:59 -0700

    feat: update stories

---
+++ packages/splits-kit/storybook/stories/CreateSplit.stories.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { CreateSplit } from '@0xsplits/splits-kit'
+import ConnectWallet from '../components/ConnectWallet'
+
+const DEFAULT_ARGS = {
+  chainId: 1,
+}
+
+const meta: Meta<typeof CreateSplit> = {
+  title: 'Components/CreateSplit',
+  component: CreateSplit,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: 'Create a split contract',
+      },
+    },
+  },
+  argTypes: {
+    chainId: {
+      description: 'Chain ID of a supported network',
+    },
+  },
+  args: DEFAULT_ARGS,
+  decorators: [
+    (Story, context) => {
+      return (
+        <ConnectWallet chainId={context.args.chainId}>
+          <Story />
+        </ConnectWallet>
+      )
+    },
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof CreateSplit>
+
+export const Basic: Story = {}
+
+export const DefaultRecipients: Story = {
+  args: {
+    defaultRecipients: [
+      {
+        address: '0xA8b2e53C70743309f8D668B52ea09158008FAf91',
+        percentAllocation: 99.0,
+      },
+      {
+        address: '0xEc8Bfc8637247cEe680444BA1E25fA5e151Ba342',
+        percentAllocation: 1.0,
+      },
+    ],
+  },
+}
+
+export const MultiChain: Story = {
+  args: {
+    chainId: 5,
+    defaultDistributorFee: 0.01,
+    defaultDistributorFeeOptions: [0.1, 1, 10],
+    defaultController: '0xA8b2e53C70743309f8D668B52ea09158008FAf91',
+    defaultRecipients: [
+      {
+        address: '0xA8b2e53C70743309f8D668B52ea09158008FAf91',
+        percentAllocation: 99.0,
+      },
+      {
+        address: '0xEc8Bfc8637247cEe680444BA1E25fA5e151Ba342',
+        percentAllocation: 1.0,
+      },
+    ],
+  },
+}
