1adf210cf9bb1d2b6f0a7eab39e6406d59fdd7cd
Jonathan Cai <caimjonathan@gmail.com>
2023-10-26 15:54:57 -0700

    chore: updated tailwindcss config purge

--- packages/splits-kit/tailwind.config.ts
+++ packages/splits-kit/tailwind.config.ts
@@ -1,11 +1,7 @@
 export default {
-  content: [],
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-  purge: {
-    enabled: process.env.NODE_ENV === 'publish',
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  },
 }
