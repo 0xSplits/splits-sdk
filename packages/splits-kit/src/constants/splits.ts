5d632ff794bc9761ad7f1d6e6154f0e99bb2d9ad
Jonathan Cai <caimjonathan@gmail.com>
2023-11-01 14:48:38 -0700

    fix: default recipients

--- packages/splits-kit/src/constants/splits.ts
+++ packages/splits-kit/src/constants/splits.ts
@@ -5,11 +5,6 @@ export const SPLIT_RECIPIENT_MAX_DECIMALS = 4
 export const SPONSORSHIP_THRESHOLD = 0.1
 
 export const EMPTY_RECIPIENT = { address: '', percentAllocation: 0 }
-export const DEFAULT_RECIPIENTS = [
-  {
-    address: '0x5090c4Fead5Be112b643BC75d61bF42339675448',
-    percentAllocation: 99.4,
-  },
-]
+export const DEFAULT_RECIPIENTS = [{ address: '', percentAllocation: 0 }]
 export const DEFAULT_DISTRIBUTOR_FEE = 0.1
 export const DEFAULT_DISTRIBUTOR_FEE_OPTIONS = [0.01, 0.1, 1]
