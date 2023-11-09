f4415d2bf4f49b933c338b69b446a075c52c3263
Jonathan Cai <caimjonathan@gmail.com>
2023-11-09 11:20:44 -0800

    feat: introduction page styles

--- packages/splits-kit/storybook/stories/Introduction.stories.tsx
+++ packages/splits-kit/storybook/stories/Introduction.stories.tsx
@@ -4,27 +4,38 @@ import { SplitsProvider } from '@0xsplits/splits-sdk-react'
 
 const Components = () => {
   return (
-    <div className="p-4 max-w-5xl mx-auto">
+    <div className="p-4 max-w-5xl mx-auto space-y-4">
       <h1 className="text-4xl">SplitsKit</h1>
       <p className="my-4">
         SplitsKit is the best way developers to add Splits-related flows into
-        their React apps.
+        their React apps. Learn more in the{' '}
+        <a
+          className="underline"
+          target="_blank"
+          href="https://docs.splits.org/splits-kit"
+        >
+          docs
+        </a>
+        .
       </p>
-
+      <div className="inline-block bg-gray-100 border border-gray-200 rounded px-2 py-0.5">
+        <code>npm install @0xsplits/splits-kit</code>
+      </div>
       <SplitsProvider>
-        <div className="grid grid-cols-2 gap-2">
+        <div className="grid grid-cols-2 gap-6">
           <div>
-            <h3 className="text-xl code">{'<CreateSplit />'}</h3>
-            <div className="h-12 my-4">
+            <h3 className="text-lg font-medium">Create Split</h3>
+            <div className="mt-2 mb-6 text-sm">
               React form component with everything needed to create a Split
               contract, including pre-filled default values and themes.
             </div>
             <CreateSplit chainId={1} width="full" />
           </div>
           <div>
-            <h3 className="text-xl code">{'<DisplaySplit />'}</h3>
-            <div className="h-12 my-4">
-              React component to display all details for a Split contract.
+            <h3 className="text-lg font-medium">Display Split</h3>
+            <div className="mt-2 mb-6 text-sm">
+              React component to display all details for a Split contract,
+              including the ability to distribute balances.
             </div>
             <DisplaySplit
               address="0xF8843981e7846945960f53243cA2Fd42a579f719"
