720c9cffc267d276c98a072d9491b4b44bcb069c
Jonathan Cai <caimjonathan@gmail.com>
2024-01-29 09:43:29 -0800

    fix(splits-kit): move import css outside of package

--- packages/splits-kit/storybook/preview.tsx
+++ packages/splits-kit/storybook/preview.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import WagmiProvider from './components/WagmiProvider'
 import { dark, light } from './theme'
 
+import '@0xsplits/splits-kit/dist/styles.css'
+
 const preview = {
   parameters: {
     darkMode: {
