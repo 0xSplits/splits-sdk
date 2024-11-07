33f2ed2cdecc69ead600504de8bfa040b38febc9
Mike H <mikeh09@gmail.com>
2024-11-06 22:39:03 -0800

    export splitsprovider in splits kit

--- packages/splits-kit/src/index.ts
+++ packages/splits-kit/src/index.ts
@@ -1,4 +1,4 @@
-import { useSplitsClient } from '@0xsplits/splits-sdk-react'
+import { useSplitsClient, SplitsProvider } from '@0xsplits/splits-sdk-react'
 
 export { default as CreateSplit } from './components/CreateSplit'
 export type { ICreateSplitProps } from './components/CreateSplit'
@@ -6,4 +6,4 @@ export type { ICreateSplitProps } from './components/CreateSplit'
 export { default as DisplaySplit } from './components/DisplaySplit'
 export type { IDisplaySplitProps } from './components/DisplaySplit'
 
-export { useSplitsClient }
+export { useSplitsClient, SplitsProvider }
