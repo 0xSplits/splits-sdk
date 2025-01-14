a96a7863a0744137bf8c2c6c81e5efeb8db8172f
Mike H <mikeh09@gmail.com>
2025-01-14 15:21:48 -0800

    remove logs

--- packages/splits-sdk/src/utils/requests.ts
+++ packages/splits-sdk/src/utils/requests.ts
@@ -449,11 +449,6 @@ export const searchLogs = async <
   let createLog: SplitCreatedLogType | undefined = undefined
   let updateLog: SplitUpdatedLogType | undefined = currentUpdateLog
 
-  console.log('IN HERE')
-  console.log(splitV2Version)
-  console.log(formattedSplitAddress)
-  console.log(publicClient.chain.id)
-
   if (splitV2Version === 'splitV2o1') {
     const splitContract = getContract({
       address: formattedSplitAddress,
