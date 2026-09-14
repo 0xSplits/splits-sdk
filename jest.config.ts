e2f7f17a7b630155ecedf507c02c40a8de7cac11
Mike H <mihoward21@users.noreply.github.com>
2026-09-14 14:27:16 -0700

    Remove testing changes from Robinhood support PR

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,6 @@ require('dotenv').config()
 
 export default {
   preset: 'ts-jest',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { downlevelIteration: true } }],
-  },
   testEnvironment: 'node',
   testPathIgnorePatterns: ['.*fork.*'],
 }
